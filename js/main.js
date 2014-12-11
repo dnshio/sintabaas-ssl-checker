2c50afd52582cde0df115d26dda7ccfb4509ebd3
Rick Makkee <rick@mediamonks.com>
2014-12-11 16:42:00 +0100

    added end message, after 2 rounds of console panel data

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $( document ).ready(function() {
 		for(var i = 0; i < content.length; i++) {
 			var rand = ((Math.floor(Math.random() * 30) + 10) * 100);
 			delay = delay + rand;
-			showEl($('#top-steps'), i, '', delay);
+			showEl($('#top-steps'), i, '', delay, false);
 
 			if(i == (content.length - 1)){
 				setTimeout(function(){
@@ -24,20 +24,22 @@ $( document ).ready(function() {
 
 	var bottomDelay = 0;
 	var consoleFrame = function() {
-		for(var i = 0; i < consoleContent.length; i++) {
-			var rand = ((Math.floor(Math.random() * 80) + 1) * 10);
-			bottomDelay = bottomDelay + rand;
-			showEl($('#console-panel'), i, 'console', bottomDelay);
+		var times = 2;
+		for(var j = 0; j < times; j++){
+			for(var i = 0; i < consoleContent.length; i++) {
+				var rand = ((Math.floor(Math.random() * 80) + 1) * 10);
+				bottomDelay = bottomDelay + rand;
 
-			if(i == (consoleContent.length - 1)){
-				setTimeout(function(){
-					consoleFrame();
-				}, 2000);
+				if(j == (times - 1) && i == (consoleContent.length - 1)) {
+					showEl($('#console-panel'), i, 'console', bottomDelay, true);
+				}else {
+					showEl($('#console-panel'), i, 'console', bottomDelay, false);
+				}
 			}
 		}
 	};
 
-	var showEl = function(el, num, type, delay) {
+	var showEl = function(el, num, type, delay, last) {
 		setTimeout(function(){
 			$(el).addClass('show');
 
@@ -47,8 +49,28 @@ $( document ).ready(function() {
 				$(el ).append('<li>' + content[num] + '</li>');
 			}
 
+			if(last) {
+				$('#console-panel li' ).css( 'opacity', '0' );
+				setTimeout(function(){
+					$('#console-panel li' ).css( 'opacity', '1' );
+					setTimeout(function(){
+						$('#console-panel' ).empty();
+						var endMessage = [
+							['We are not sure...'],
+						    ['Yes']
+						];
+						$(el ).append('<li class="error">' + endMessage[Math.floor(Math.random()*endMessage.length)] + '</li>');
+					}, 200);
+				}, 200);
+			}
+
 			topPanel.scrollTop = topPanel.scrollHeight;
 			consolePanel.scrollTop = consolePanel.scrollHeight;
 		}, delay);
 	};
+
+	var endMessage = [
+		['We are not sure...'],
+		['Yes']
+	];
 });
